26ac69bc2e71507eafdc1e89d8a0f4969502b4bc
Enrique Mireles <enriquemg96@gmail.com>
2022-08-09 23:12:28 +0200

    Calculate distance between two GPS coordinates

--- Digital-Compass.ino
+++ Digital-Compass.ino
@@ -1,22 +1,33 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_GPS.h>
+#include <Adafruit_SH110X.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SH110X.h>
-#include <Adafruit_GPS.h>
 
 #define GPSSerial Serial1
 
-#define BUTTON_A  9
-#define BUTTON_B  6
-#define BUTTON_C  5
+#define BUTTON_A 9
+#define BUTTON_B 6
+#define BUTTON_C 5
 
 Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
 Adafruit_GPS GPS(&GPSSerial);
 
 uint32_t timer = millis();
 
-void setup() {
+float distanceBetween(float lat1, float lon1, float lat2, float lon2) {
+  // Uses the haversine formulat to calculate the distance between two
+  // coordinates in meters. The coordinates must be in degrees.
 
+  float dlon = radians(lon2 - lon1);
+  float dlat = radians(lat2 - lat1);
+  float a = sq(sin(dlat / 2)) + cos(radians(lat1)) * cos(radians(lat2)) * sq(sin(dlon / 2));
+  float c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+  return c * 6371000.0;  // Radius of the Earth.
+}
+
+void setup() {
   // Serial init
   Serial.begin(115200);
   Serial.println("Display GPS data to OLED display");
@@ -24,11 +35,11 @@ void setup() {
   // GPS init
   GPS.begin(9600);
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate.
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);  // 1 Hz update rate.
   delay(1000);
 
   // OLED init
-  display.begin(0x3C, true); 
+  display.begin(0x3C, true);
   display.setRotation(1);
   display.setTextSize(1);
   display.setTextColor(SH110X_WHITE);
@@ -40,28 +51,43 @@ void loop() {
   GPS.read();
 
   if (GPS.newNMEAreceived()) {
+    Serial.println(GPS.lastNMEA());
     GPS.parse(GPS.lastNMEA());
   }
 
   if (millis() - timer > 2000) {
     timer = millis();
-  
+
     display.clearDisplay();
-    display.setCursor(0,0);
+    display.setCursor(0, 0);
 
     // Line 0, fix status, quality, and satellite count.
-    display.print("Fix: "); display.print((int)GPS.fix);
-    display.print(" Q: "); display.print((int)GPS.fixquality);
-    display.print(" Sat: "); display.println((int)GPS.satellites);
+    display.print("Fix: ");
+    display.print((int)GPS.fix);
+    display.print(" Q: ");
+    display.print((int)GPS.fixquality);
+    display.print(" Sat: ");
+    display.println((int)GPS.satellites);
 
     // Line 1, latitude.
-    display.print("Lat: "); display.print(GPS.latitude, 4); display.println(GPS.lat);
+    display.print("Lat: ");
+    display.println(GPS.latitudeDegrees, 6);
+    // display.print(GPS.latitude, 4);
+    // display.println(GPS.lat);
 
     // Line 2, longitude.
-    display.print("Lon: "); display.print(GPS.longitude, 4); display.println(GPS.lon);
+    display.print("Lon: ");
+    display.println(GPS.longitudeDegrees, 6);
+    // display.print(GPS.longitude, 4);
+    // display.println(GPS.lon);
 
     // Line 3, altitude.
-    display.print("Alt: "); display.println(GPS.altitude);
+    display.print("Alt: ");
+    display.println(GPS.altitude, 2);
+
+    // Line 4. distance.
+    display.print("Dist: ");
+    display.println(distanceBetween(GPS.latitudeDegrees, GPS.longitudeDegrees, 52.4771458, 13.4220666));
 
     display.display();
   }
--- README.md
+++ README.md
@@ -15,3 +15,5 @@
 [1](https://learn.adafruit.com/adafruit-feather-m0-basic-proto/setup)
 [2](https://learn.adafruit.com/adafruit-128x64-oled-featherwing)
 [3](https://learn.adafruit.com/adafruit-ultimate-gps-featherwing)
+[4](https://www.movable-type.co.uk/scripts/latlong.html)
+[5](https://geodesyapps.ga.gov.au/vincenty-inverse)
